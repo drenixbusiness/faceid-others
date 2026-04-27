7e320048ec7eb27764ca8e23f526a19387bb01ad
drenixbusiness <drenixit@gmail.com>
2026-04-28 02:26:25 +0500

    fix dotenv path for PM2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').join(__dirname, '.env') });
 const express = require('express');
 const axios = require('axios');
 const Database = require('better-sqlite3');
---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apps: [{
+    name: 'hikvision-other',
+    script: './app.js',
+    cwd: '/home/faceid/faceid-others'
+  }]
+};
